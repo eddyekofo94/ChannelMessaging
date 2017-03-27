4e8082842ddc637cf6519f5609b37c1dc9dfcc71
Eddy Ekofo <eddyecko@gmail.com>
2017-03-27 13:06:51 +0200

    Working on the amis SQLLite

---
+++ app/src/main/java/com/eddy/ifukie/channelmessaging/FriendsDB.java
@@ -0,0 +1,10 @@
+package com.eddy.ifukie.channelmessaging;
+
+import SQLiteOpenHelper;
+
+/**
+ * Created by eddyekofo on 27/03/2017.
+ */
+
+public class FriendsDB extends SQLiteOpenHelper {
+}
---
+++ app/src/main/java/com/eddy/ifukie/channelmessaging/UserDatasource.java
@@ -0,0 +1,8 @@
+package com.eddy.ifukie.channelmessaging;
+
+/**
+ * Created by eddyekofo on 27/03/2017.
+ */
+
+public class UserDatasource {
+}
